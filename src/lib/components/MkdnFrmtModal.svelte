5b2f17e7e326d2865023d2f63f7fe7f4edc79044
manunamz <manunamz@pm.me>
2024-01-16 15:22:55 -0500

    fix: updated mkdn format option text.

--- src/lib/components/MkdnFrmtModal.svelte
+++ src/lib/components/MkdnFrmtModal.svelte
@@ -85,8 +85,8 @@
             <select id="wikiSelect"
                     class="block appearance-none w-full bg-white text-black border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                     bind:value={$mkdnFrmt.textKind}>
-              <option value="regular text">regular text</option>
-              <option value="[[wiki text]]">[[wiki text]]</option>
+              <option value="plain-text">plain-text</option>
+              <option value="[[wikitext]]">[[wikitext]]</option>
             </select>
           </div>
           <!-- Case Dropdown -->
