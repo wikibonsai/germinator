dd7138a80cb35905aee7263beb457daa5422bb0e
manunamz <manunamz@pm.me>
2024-06-04 12:47:51 -0400

    fix: env vars again.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,24 @@
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
-import { OPENAI_API_KEY } from '$env/static/private';
 
 
 export const prerender: boolean = false;
 
 // ref: https://scottspence.com/posts/passing-sveltekit-page-server-js-data-to-page-js
+
 export const load: PageServerLoad = async ({ params }) => {
-  return {
-    apiKey: OPENAI_API_KEY ?? '',
-  };
+  let apiKey = '';
+
+  try {
+    const env = await import('$env/static/private');
+    apiKey = env.OPENAI_API_KEY || '';
+  } catch (e) {
+    // Handle the case when the environment variable is not defined
+    console.warn('OPENAI_API_KEY is not defined');
+  }
+
   // throw error(404, 'Page not found');
-}
+  return {
+    apiKey,
+  };
+};
