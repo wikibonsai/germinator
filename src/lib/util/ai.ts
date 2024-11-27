1bf5293a3146ff17553c836c2f2ba07fc9fcd1e9
manunamz <manunamz@pm.me>
2024-11-27 16:22:07 -0500

    ref: simplify ai.ts and add some extra prompts.

--- src/lib/util/ai.ts
+++ src/lib/util/ai.ts
@@ -2,137 +2,142 @@
 
 import type { Opts, OptLLM } from './$types';
 import { URL_API_ANTHROPIC, URL_API_OPENAI, URL_API_XAI } from './const';
-import { formatPrompt, SYSTEM_PROMPT, PEPTALK } from './prompt';
-
+import { formatPrompt, IGNORE_LAZY, SYSTEM_PROMPT, TAKE_TIME, PEPTALK } from './prompt';
 
 function validate(opts: OptLLM): void {
   if ((opts.provider !== 'anthropic') && (opts.provider !== 'openai') && (opts.provider !== 'xai')) {
-    console.error('invalid provider: ', opts);
     throw new Error(
       'You need to provide a valid llm provider. Make sure a valid provider is selected in your options.'
-    )
+    );
   }
   const selectedProvider: string = opts.provider;
   if (opts[selectedProvider].apiKey === '') {
-    console.error('invalid api key: ', opts);
     throw new Error(
       'You need to provide an API key. Make sure valid api keys are set in your options.'
-    )
+    );
   }
 }
 
 export async function germinate(userMessage: string, opts: Opts): Promise<string> {
-  console.debug('germinating with opts: ', opts);
+  console.log('germinating with opts: ', opts);
   try {
     validate(opts.llm);
   } catch (e) {
     console.error('germinate error: ', e);
     return e.message;
   }
-  // first, we build the prompt that we'll send to openai.
-  const seedPrompt: string = SYSTEM_PROMPT + formatPrompt(opts.mkdn) + PEPTALK;
-  let responseStr: string = '';
+  const seedPrompt: string = SYSTEM_PROMPT
+                            + formatPrompt(opts.mkdn)
+                            + IGNORE_LAZY
+                            + TAKE_TIME
+                            + PEPTALK;
   console.debug('seed prompt:', seedPrompt);
   console.debug('sending concept seed to llm...');
-  let body: JSON;
-  let responseJSON: JSON;
+  let request: RequestLLM | undefined = undefined;
   if (opts.llm.provider === 'anthropic') {
-    body = {
-      model: opts.llm.anthropic.model,
-      max_tokens: 4096,
-      messages: [
-        { role: 'user', content: "SEED PROMPT:\n\n" + seedPrompt + "\n\nUSER MSG:\n\n" + userMessage }
-      ],
-    };
-
-    try {
-      const response = await fetch(URL_API_ANTHROPIC, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'anthropic-version': '2023-06-01',
-          'x-api-key': opts.llm.anthropic.apiKey,
-        },
-        body: JSON.stringify(body),
-      });
-      const responseJSON = await response.json();
-      if (!response.ok) {
-        throw new Error(responseJSON.error?.message || 'An error occurred with the Anthropic API');
-      }
-      responseStr = responseJSON.content[0].text;
-      console.debug('claude response payload: ', responseJSON);
-      console.debug('claude response string: ', responseStr);
-    } catch (e) {
-      console.error(e);
-      throw new Error('Sorry, there was an error fetching from ' + opts.llm.provider);
-    }
+    request = prepAnthropic(opts.llm.anthropic.apiKey, opts.llm.anthropic.model, seedPrompt, userMessage);
   }
   if (opts.llm.provider === 'openai') {
-    body = {
-      model: opts.llm.openai.model,
+    request = prepOpenAI(opts.llm.openai.apiKey, opts.llm.openai.model, seedPrompt, userMessage);
+  }
+  if (opts.llm.provider === 'xai') {
+    request = prepXAI(opts.llm.xai.apiKey, opts.llm.xai.model, seedPrompt, userMessage);
+  }
+  const errorString: string = 'An error occurred with the LLM API';
+  if (request === undefined) {
+    return errorString;
+  }
+  let responseJSON: JSON;
+  let responseStr: string;
+  try {
+    const response = await fetch(request.url, {
+      method: 'POST',
+      headers: request.headers,
+      body: JSON.stringify(request.body),
+    });
+    responseJSON = await response.json();
+    if (!response.ok) {
+      throw new Error(responseJSON.error?.message || errorString);
+    }
+    // result format handling per provider
+    if (opts.llm.provider === 'anthropic') {
+      responseStr = responseJSON.content[0].text;
+    // openai + xai
+    } else {
+      responseStr = responseJSON.choices[0].message.content;
+    }
+  } catch (e) {
+    console.error(e);
+    throw new Error('Sorry, there was an error fetching from ' + opts.llm.provider);
+  }
+  console.debug('response payload: ', responseJSON);
+  console.debug('response string: ', responseStr);
+  return responseStr;
+}
+
+interface RequestLLM {
+  headers: any;
+  body: JSON;
+  url: string;
+}
+
+function prepAnthropic(
+  apiKey: string,
+  model: string,
+  seedPrompt: string,
+  userMessage: string,
+): RequestLLM {
+  return {
+    headers: {
+      'Content-Type': 'application/json',
+      'anthropic-version': '2023-06-01',
+      'x-api-key': apiKey,
+    },
+    body: {
+      model: model,
+      max_tokens: 4096,
+      messages: [
+        {
+          role: 'user',
+          content: "SEED PROMPT:\n\n" + seedPrompt
+                  + "\n\nUSER MSG:\n\n" + userMessage,
+        }
+      ],
+    },
+    url: URL_API_ANTHROPIC,
+  };
+}
+
+function prepOpenAI(
+  apiKey: string,
+  model: string,
+  seedPrompt: string,
+  userMessage: string,
+): RequestLLM {
+  return {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: {
+      model: model,
       max_tokens: 4096,
       temperature: 0,
       messages: [
         { role: 'system', content: seedPrompt },
         { role: 'user', content: userMessage },
       ],
-    };
-    // make a request to openai. `fetchFromOpenAi` is a next.js server action,
-    // so our api key is hidden.
-    try {
-      const response = await fetch(URL_API_OPENAI, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${opts.llm.openai.apiKey}`,
-        },
-        body: JSON.stringify(body),
-      });
-      responseJSON = await response.json();
-    } catch (e) {
-      console.error(e, opts.llm.openai.apiKey);
-      throw new Error('Sorry, there was an error fetching from ' + opts.llm.provider)
-    }
-    if (responseJSON.error) {
-      responseStr = responseJSON.error.message;
-    } else {
-      responseStr = responseJSON.choices[0].message.content;
-    }
-    console.debug('chatgpt response payload: ', responseJSON);
-    console.debug('chatgpt response string: ', responseStr);
-  }
-  if (opts.llm.provider === 'xai') {
-    const request = prepXAI(opts.llm.xai.apiKey, opts.llm.xai.model, seedPrompt, userMessage);
-
-    try {
-      const response = await fetch(request.url, {
-        method: 'POST',
-        headers: request.headers,
-        body: JSON.stringify(request.body),
-      });
-      responseJSON = await response.json();
-      if (!response.ok) {
-        throw new Error(responseJSON.error?.message || 'An error occurred with the XAI API');
-      }
-      responseStr = responseJSON.choices[0].message.content;
-      console.debug('xai response payload: ', responseJSON);
-      console.debug('xai response string: ', responseStr);
-    } catch (e) {
-      console.error(e);
-      throw new Error('Sorry, there was an error fetching from ' + opts.llm.provider);
-    }
-  }
-  console.debug('germinate response string: ', responseStr);
-  return responseStr;
+    },
+    url: URL_API_OPENAI,
+  };
 }
 
-export function prepXAI(
+function prepXAI(
   apiKey: string,
   model: string,
   seedPrompt: string,
   userMessage: string,
-) {
-  // from: https://docs.x.ai/docs/quickstart#making-your-first-request
+): RequestLLM {
   return {
     headers: {
       'Content-Type': 'application/json',
@@ -148,5 +153,5 @@ export function prepXAI(
       ],
     },
     url: URL_API_XAI,
-  }
+  };
 }
